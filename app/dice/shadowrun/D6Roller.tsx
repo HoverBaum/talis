e9575e0c62244121c3014dadd49ef6f31cef8755
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-07-26 15:56:15 +0200

    🚧 hardcoded config.

--- app/dice/shadowrun/D6Roller.tsx
+++ app/dice/shadowrun/D6Roller.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from 'react'
 import { DiceRollResult } from './DiceRollResult'
 import { DiceSelectWheel } from './DiceSelectWheel'
+import { useConfig } from './useConfig'
+import { FreeDiceInput } from './FreeDiceInput'
 
 export type DiceRollType = {
   results: number[]
@@ -22,6 +24,9 @@ const DEFAULT_DICE_AMOUNT = 7
 export const D6Roller = () => {
   const [results, setResults] = useState<DiceRollType[]>([])
   const [numberOfDice, setNumberOfDice] = useState<number>(DEFAULT_DICE_AMOUNT)
+  const {
+    config: { showNewResultBottom, useFreeInput },
+  } = useConfig()
 
   const rollD6 = (dice: number) => {
     console.log(`Rolling ${dice} dice.`)
@@ -83,98 +88,14 @@ export const D6Roller = () => {
         </div>
       </div>
       <div className="flex-none border-t-2 pt-4">
-        <div className="flex">
-          <p>
-            Number of dice:{' '}
-            <input
-              type="text"
-              placeholder="0"
-              className="input input-bordered w-14 text-center"
-              value={numberOfDice > 0 ? numberOfDice : ''}
-              onChange={(e) => {
-                if (isNaN(parseInt(e.target.value))) return setNumberOfDice(0)
-                setNumberOfDice(parseInt(e.target.value))
-              }}
-            />
-          </p>
-
-          <button
-            className="btn btn-square ml-2"
-            disabled={numberOfDice <= 1}
-            onClick={() =>
-              setNumberOfDice((dice) => {
-                if (dice <= 1) {
-                  return dice
-                } else {
-                  return dice - 1
-                }
-              })
-            }
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 12h-15"
-              />
-            </svg>
-          </button>
-          <button
-            className="btn btn-square ml-2"
-            disabled={numberOfDice >= MAX_DICE_AMOUNT}
-            onClick={() =>
-              setNumberOfDice((dice) => {
-                if (dice >= MAX_DICE_AMOUNT) {
-                  return dice
-                } else {
-                  return dice + 1
-                }
-              })
-            }
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </button>
-
-          <button
-            className="btn btn-square ml-2"
-            onClick={() => setResults([])}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
-          </button>
-        </div>
+        {useFreeInput && (
+          <FreeDiceInput
+            numberOfDice={numberOfDice}
+            onNewNumber={setNumberOfDice}
+            onClearResults={() => setResults([])}
+            maxDiceAmount={MAX_DICE_AMOUNT}
+          />
+        )}
 
         <button
           disabled={numberOfDice <= 0}
---
+++ app/dice/shadowrun/useConfig.ts
@@ -0,0 +1,9 @@
+export const useConfig = () => {
+  const config = {
+    showNewResultBottom: true,
+    useFreeInput: true,
+    useQuickButtons: true,
+  }
+
+  return { config }
+}
