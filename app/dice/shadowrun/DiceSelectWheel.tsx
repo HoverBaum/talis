efc886c15d25b1f0b1d5555c814380676c3d67ad
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-07-27 00:19:09 +0200

    🐛 always use last scrolled to number.
    No longer using scrollend.

--- app/dice/shadowrun/DiceSelectWheel.tsx
+++ app/dice/shadowrun/DiceSelectWheel.tsx
@@ -7,6 +7,8 @@ type DiceSelectWheelProps = {
   onChange: (value: number) => void
 }
 
+let timeout: any = null
+
 // A horizontal wheel of dice numbers that can be selected.
 // The user can spin throught he wheel to select a number.
 export const DiceSelectWheel = ({
@@ -54,29 +56,45 @@ export const DiceSelectWheel = ({
 
   // Setup scroll listeners for wheel container.
   useEffect(() => {
-    const wheelContainer = document.getElementById('wheelContainer')
-    if (wheelContainer) {
-      wheelContainer.addEventListener('scrollend', (event) => {
-        // Find the element currently scrolled into the center of the wheel.
-        // We look at all elements and find the one in the center using the scrollTop of the container.
-        const elements =
-          document.querySelectorAll<HTMLDivElement>('[data-number]')
-        const center = wheelContainer.scrollTop + height / 2
-        let closestElement: HTMLDivElement | null = null
-        let closestDistance = Number.MAX_SAFE_INTEGER
-        elements.forEach((element) => {
-          const distance = Math.abs(center - element.offsetTop)
-          if (distance < closestDistance) {
-            closestElement = element
-            closestDistance = distance
-          }
-        })
-        // If we found an element, trigger change event.
-        if (closestElement as HTMLDivElement | null) {
-          const number = Number(closestElement!.dataset.number)
-          onChange(number)
+    const findCurrentEvent = (container: HTMLElement) => {
+      // Find the element currently scrolled into the center of the wheel.
+      // We look at all elements and find the one in the center using the scrollTop of the container.
+      const elements =
+        document.querySelectorAll<HTMLDivElement>('[data-number]')
+      const center = container.scrollTop + height / 2
+      let closestElement: HTMLDivElement | null = null
+      let closestDistance = Number.MAX_SAFE_INTEGER
+      elements.forEach((element) => {
+        const distance = Math.abs(center - element.offsetTop)
+        if (distance < closestDistance) {
+          closestElement = element
+          closestDistance = distance
         }
       })
+      // If we found an element, trigger change event.
+      if (closestElement as HTMLDivElement | null) {
+        const number = Number(closestElement!.dataset.number)
+        onChange(number)
+      }
+    }
+
+    const scrollListener = () => {
+      timeout && clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        const wheelContainer = document.getElementById('wheelContainer')
+        if (wheelContainer) {
+          findCurrentEvent(wheelContainer)
+        }
+      }, 100)
+    }
+
+    const wheelContainer = document.getElementById('wheelContainer')
+    if (wheelContainer) {
+      wheelContainer.addEventListener('scroll', scrollListener)
+
+      return () => {
+        wheelContainer.removeEventListener('scroll', scrollListener)
+      }
     }
   }, [height, onChange])
 
