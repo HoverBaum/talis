57d099d7b7c5e198483787a65d8a46a43bdcf7b2
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-07-31 11:46:31 +0200

    ✨ link manifest.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         suppressHydrationWarning={true}
         className={`${inter.className} min-h-screen bg-base-100`}
