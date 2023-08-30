31eefadd13f1c88af1794b2372b99bd508371053
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-08-30 14:38:23 +0200

    🔧 fix imports.

--- app/[lang]/dice/shadowrun/shadowrunSlice.ts
+++ app/[lang]/dice/shadowrun/shadowrunSlice.ts
@@ -2,7 +2,7 @@
 
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { DiceRollType } from './page'
+import { DiceRollType } from './DiceRollResult'
 
 const MAX_DICE_AMOUNT = 50
 
