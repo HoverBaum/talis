d51116a728f7a4b599471ce1da3c37cd3c0e145b
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-11-17 01:14:44 +0100

    ✨ display mdx pages

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,7 @@
-import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 import { i18n } from '@/i18n/config'
-import { ThemeProvider } from '@/components/theme-provider'
-import { AppSidebar } from '@/components/app-sidebar'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
+import { ProvidersWrapper } from '@/components/providers-wrapper'
 
 export function generateStaticParams() {
   return i18n.locales.map((locale) => ({ locale }))
@@ -30,21 +22,6 @@ export default async function LocaleLayout({
 
   const messages = await getMessages()
 
-  return (
-    <NextIntlClientProvider messages={messages}>
-      <ThemeProvider defaultTheme="system" storageKey="talis-theme">
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-            </header>
-            <main className="flex-1">{children}</main>
-          </SidebarInset>
-        </SidebarProvider>
-      </ThemeProvider>
-    </NextIntlClientProvider>
-  )
+  return <ProvidersWrapper messages={messages} locale={locale}>{children}</ProvidersWrapper>
 }
 
---
+++ components/providers-wrapper.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { NextIntlClientProvider } from 'next-intl'
+import { ThemeProvider } from './theme-provider'
+import { AppSidebar } from './app-sidebar'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from './ui/sidebar'
+import { Separator } from './ui/separator'
+
+type ProvidersWrapperProps = {
+  messages: any
+  locale: string
+  children: React.ReactNode
+}
+
+// This component must be a client component to use contexts
+export function ProvidersWrapper({ messages, locale, children }: ProvidersWrapperProps) {
+  return (
+    <NextIntlClientProvider messages={messages} locale={locale}>
+      <ThemeProvider defaultTheme="system" storageKey="talis-theme">
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+            </header>
+            <main className="flex-1">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
+      </ThemeProvider>
+    </NextIntlClientProvider>
+  )
+}
+
