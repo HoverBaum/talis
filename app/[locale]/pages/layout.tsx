f1751684169731939a72b7fb462967e0d5d841a0
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-11-20 00:51:18 +0100

    🎨 more space for pages

--- app/[locale]/pages/layout.tsx
+++ app/[locale]/pages/layout.tsx
@@ -4,7 +4,7 @@ export default function AboutLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="p-2 md:p-8 max-w-4xl mx-auto prose prose-sm md:prose-base dark:prose-invert ">
+    <div className="p-4 md:p-8 max-w-4xl mx-auto prose prose-sm md:prose-base dark:prose-invert ">
       {children}
     </div>
   )
