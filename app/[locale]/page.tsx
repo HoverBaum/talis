72e35286abbdfafaabf99cf78e771f115bd8f445
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-11-20 00:53:22 +0100

    📚 update translations

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -39,46 +39,46 @@ export default async function HomePage({
                     <p className="text-muted-foreground">{t('description')}</p>
                 </div>
 
-            <div className="grid gap-4 md:grid-cols-3 mt-8">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>{navT('shadowrun')}</CardTitle>
-                        <CardDescription>Shadowrun dice roller</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <Button asChild>
-                            <Link href={`/${locale}/dice/shadowrun`}>{navT('shadowrun')}</Link>
-                        </Button>
-                    </CardContent>
-                </Card>
+                <div className="grid gap-4 md:grid-cols-3 mt-8">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>{navT('shadowrun')}</CardTitle>
+                            <CardDescription>{t('shadowrunDescription')}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Button asChild>
+                                <Link href={`/${locale}/dice/shadowrun`}>{navT('shadowrun')}</Link>
+                            </Button>
+                        </CardContent>
+                    </Card>
 
-                <Card>
-                    <CardHeader>
-                        <CardTitle>{navT('d6')}</CardTitle>
-                        <CardDescription>General purpose D6 roller</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <Button asChild>
-                            <Link href={`/${locale}/dice/d6`}>{navT('d6')}</Link>
-                        </Button>
-                    </CardContent>
-                </Card>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>{navT('d6')}</CardTitle>
+                            <CardDescription>{t('d6Description')}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Button asChild>
+                                <Link href={`/${locale}/dice/d6`}>{navT('d6')}</Link>
+                            </Button>
+                        </CardContent>
+                    </Card>
 
-                <Card>
-                    <CardHeader>
-                        <CardTitle>{navT('daggerheart')}</CardTitle>
-                        <CardDescription>Daggerheart dice roller</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <Button asChild>
-                            <Link href={`/${locale}/dice/daggerheart`}>
-                                {navT('daggerheart')}
-                            </Link>
-                        </Button>
-                    </CardContent>
-                </Card>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>{navT('daggerheart')}</CardTitle>
+                            <CardDescription>{t('daggerheartDescription')}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Button asChild>
+                                <Link href={`/${locale}/dice/daggerheart`}>
+                                    {navT('daggerheart')}
+                                </Link>
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
-        </div>
         </>
     )
 }
