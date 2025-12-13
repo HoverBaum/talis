231d40fd3806f077931f67a668a3c7fa7a6085e8
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-12-14 00:43:44 +0100

    ✨ posthog for errors

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,21 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // PostHog rewrites
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // Required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 }
 
 const withMDX = createMDX({
