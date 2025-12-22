8c0e411031c791faae86f14384cb42b1c199813a
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-12-22 14:16:34 +0100

    🔧 PostHog really only in prod.

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,15 @@
-import posthog from "posthog-js"
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
-})
+// Only load PostHog on the client in production
+if (process.env.NODE_ENV === 'production') {
+  void (async () => {
+    const posthog = (await import('posthog-js')).default
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: '/ingest',
+      ui_host: 'https://eu.posthog.com',
+      defaults: '2025-05-24',
+      capture_exceptions: true,
+      debug: false,
+    })
+  })()
+} else {
+  console.log('PostHog not initialized due to env not being production.')
+}
