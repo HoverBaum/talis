631b508652928ea94d1e120cdce4cafa05b36231
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2026-01-06 21:08:46 +0100

    🔥 more cleanup

--- app/[locale]/pages/layout.tsx
+++ app/[locale]/pages/layout.tsx
@@ -1,5 +1,3 @@
-import { MDXDebugLogger } from './_components/MDXDebugLogger'
-
 export default function AboutLayout({
   children,
 }: {
@@ -7,7 +5,6 @@ export default function AboutLayout({
 }) {
   return (
     <div className="prose prose-sm md:prose-base p-4 md:p-8 max-w-3xl mx-auto">
-      <MDXDebugLogger />
       {children}
     </div>
   )
