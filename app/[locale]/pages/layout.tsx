7a31e62d38af8907b782ea0c496d9ede7a36e137
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2026-01-06 21:10:04 +0100

    🔧 remove prose from all pages

--- app/[locale]/pages/layout.tsx
+++ app/[locale]/pages/layout.tsx
@@ -4,7 +4,7 @@ export default function AboutLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="prose prose-sm md:prose-base p-4 md:p-8 max-w-3xl mx-auto">
+    <div className="p-4 md:p-8 max-w-3xl mx-auto">
       {children}
     </div>
   )
